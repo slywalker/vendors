022dc5395d2a686c18cabccad8db78f1a204bb9d
Yasuo Harada <slywalker.net@gmail.com>
2009-07-25 14:23:18 +0900

    add invalidate() for i18n

--- shells/templates/skel/app_model.php
+++ shells/templates/skel/app_model.php
@@ -1,5 +1,11 @@
 <?php
 class AppModel extends Model {
 	public $actsAs = array('Containable');
+
+	//Validation message i18n
+	function invalidate($field, $value = true){
+		parent::invalidate($field, $value);
+		$this->validationErrors[$field] = __($value, true);
+	}
 }
 ?>
