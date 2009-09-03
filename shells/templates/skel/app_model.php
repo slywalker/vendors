2c508571ab38e2ee0722339608c590b7ee0acf5d
Yasuo Harada <slywalker.net@gmail.com>
2009-09-03 18:05:28 +0900

    add find

--- shells/templates/skel/app_model.php
+++ shells/templates/skel/app_model.php
@@ -3,8 +3,22 @@ class AppModel extends Model {
 	public $recursive = -1;
 	public $actsAs = array('Containable');
 
+	public function find($type, $options = array()) {
+		$method = null;
+		if(is_string($type)) {
+			$method = sprintf('__find%s', Inflector::camelize($type));
+		}
+
+		if($method && method_exists($this, $method)) {
+			return $this->{$method}($options);
+		} else {
+			$args = func_get_args();
+		}
+		return call_user_func_array(array('parent', 'find'), $args);
+	}
+
 	//Validation message i18n
-	function invalidate($field, $value = true){
+	public function invalidate($field, $value = true){
 		parent::invalidate($field, $value);
 		$this->validationErrors[$field] = __($value, true);
 	}
